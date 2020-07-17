e63bc85e5dd2d0deab442cb574140961fa0f9eaa
bkunzhang <dreamkunk@foxmail.com>
2020-07-17 17:52:03 +0800

    mongo

---
+++ code/mongodb/src/main/java/com/mongo/ChangeStatusTask.java
@@ -0,0 +1,9 @@
+package com.mongo;
+
+/**
+ * @author bingkun_zhang
+ * @date 2020/7/3
+ * 定时改变状态：把审批中改成已完成
+ */
+public class ChangeStatusTask {
+}
--- code/mongodb/src/main/java/com/mongo/ObjectIdToDate.java
+++ code/mongodb/src/main/java/com/mongo/ObjectIdToDate.java
@@ -1,3 +1,5 @@
+package com.mongo;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
