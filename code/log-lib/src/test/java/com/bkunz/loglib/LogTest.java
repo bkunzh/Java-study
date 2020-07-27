15c3587246f07ac5093dae9cf95de7407f2df5b6
bkunzhang <dreamkunk@foxmail.com>
2020-07-27 10:13:47 +0800

    多个log库

---
+++ code/log-lib/src/test/java/com/bkunz/loglib/LogTest.java
@@ -0,0 +1,17 @@
+package com.bkunz.loglib;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author bingkun_zhang
+ * @date 2020/7/27
+ */
+public class LogTest {
+    @Test
+    public void t1() {
+        Logger logger = LoggerFactory.getLogger(Object.class);
+        logger.info("log info t");
+    }
+}
